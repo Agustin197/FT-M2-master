bf6371f9dc6e2ac90b67a06992cc2ef766a8ce60
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2022-11-15 18:33:37 -0300

    hw

--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
+ const [city, setCity] = useState("")
+ 
+ const onChange = (evento) => {
+  setCity(evento.target.value)
+ }
+ 
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
+      setCity("")
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
+        value={city}
+        onChange={(evento) => onChange(evento)}
       />
       <input type="submit" value="Agregar" />
     </form>
